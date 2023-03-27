96b6eadeae78a211b4ff773eddf8c0db6983a126
Shani Israelov <56839113+shani1610@users.noreply.github.com>
2023-03-27 15:09:13 +0100

    Update opencv-omp-basic-color.cpp

--- openmp_based_image_processing/opencv-omp-basic-color.cpp
+++ openmp_based_image_processing/opencv-omp-basic-color.cpp
@@ -10,11 +10,15 @@ int main( int argc, char** argv )
 
   cv::Mat_<cv::Vec3b> source = cv::imread ( argv[1], cv::IMREAD_COLOR);
   cv::Mat_<cv::Vec3b> destination ( source.rows, source.cols );
+  cv::Mat_<cv::Vec3b> destination_anaglyph ( source.rows, source.cols/2 );
 
   cv::imshow("Source Image", source );
 
   auto begin = chrono::high_resolution_clock::now();
-  const int iter = 500;
+
+  const int iter = 1;
+  int kernel_size_div2 = 5;
+  double sigma = 1.0;
 
   for (int it=0;it<iter;it++)
     {
@@ -22,15 +26,54 @@ int main( int argc, char** argv )
       for (int i=0;i<source.rows;i++)
 	      for (int j=0;j<source.cols;j++)
           for (int c=0;c<3;c++)
-            {
-	            destination(i,j)[c] = 255.0*cos((255-source(i,j)[c])/255.0);
-            }
+          {
+            double gaus_sum = 1;
+            double dest_tmp = 0; 
+            for (int ii=-1*kernel_size_div2; ii<=kernel_size_div2; ii++)
+              for (int jj=-1*kernel_size_div2; jj<=kernel_size_div2; jj++)
+              {
+                double gaus_mult_term = 1.0 / 2.0 * M_PI * pow(sigma,2);
+                double gaus_exp_term = -1.0 * ((pow(ii,2))+(pow(jj,2))) / (2.0*pow(sigma,2));
+                double gaus = gaus_mult_term * exp(gaus_exp_term);
+                gaus_sum += gaus;
+                double thing = gaus*source(i+ii,j+jj)[c];
+                dest_tmp += thing;
+              }
+            destination(i,j)[c] = dest_tmp/gaus_sum;
+          }
     }
+
+  // Anaglyph:
+  cout << "Width : " << source.size().width << endl;
+  cout << "Height: " << source.size().height << endl;
+  int width = source.size().width;
+  int height = source.size().height;
+
+  for (int it=0;it<iter;it++)
+    {
+	 #pragma omp parallel for
+      for (int i=0;i<source.rows;i++)
+	      for (int j=0;j<source.cols/2;j++)
+        {
+            double r1 = source(i,j)[2];
+            double g1 = source(i,j)[1];
+            double b1 = source(i,j)[0];
+            double r2 = source(i,j+source.cols/2)[2];
+            double g2 = source(i,j+source.cols/2)[1];
+            double b2 = source(i,j+source.cols/2)[0];
+            destination_anaglyph(i,j)[0] = 0.249*r2+0.587*g2+0.114*b2; // b
+            destination_anaglyph(i,j)[1] = 0; // g
+            destination_anaglyph(i,j)[2] = 0.249*r1+0.587*g1+0.114*b1; // r
+        }
+    }
+
+
 
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = end-begin;
 
-  cv::imshow("Processed Image", destination );
+  cv::imshow("Blur Image", destination );
+  cv::imshow("Anaglyph Image", destination_anaglyph );
 
   cout << "Total time: " << diff.count() << " s" << endl;
   cout << "Time for 1 iteration: " << diff.count()/iter << " s" << endl;
