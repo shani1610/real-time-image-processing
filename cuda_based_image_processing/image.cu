655f18e970df54c82ceecdbbb76b997b8462ac59
Shani Israelov <56839113+shani1610@users.noreply.github.com>
2023-03-28 15:30:43 +0200

    Update image.cu

--- cuda_based_image_processing/image.cu
+++ cuda_based_image_processing/image.cu
@@ -17,10 +17,27 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<u
   
   if (dst_x < cols && dst_y < rows)
     {
-      uchar3 val = src(dst_y, dst_x);
-      dst(dst_y, dst_x).x = 255-val.x;
-      dst(dst_y, dst_x).y = 255-val.y;
-      dst(dst_y, dst_x).z = 255-val.z;
+      double sum_x = 0.0;
+      double sum_y = 0.0;
+      double sum_z = 0.0;
+      double factor = 0.0;
+      double sigma = 1.0;
+      int kernel_size_div2 = 3;
+      for (int i = -1*kernel_size_div2; i<kernel_size_div2+1; i++)
+        for (int j = -1*kernel_size_div2; j<kernel_size_div2+1; j++) 
+        {
+          uchar3 val = src(dst_y+j, dst_x+i);
+          double gaus_mult_term = 1.0 / 2.0 * M_PI * pow(sigma,2);
+          double gaus_exp_term = -1.0 * ((pow(i,2))+(pow(j,2))) / (2.0*pow(sigma,2));
+          double gaus = gaus_mult_term * exp(gaus_exp_term);
+          factor += gaus;
+          sum_x += gaus*val.x;
+          sum_y += gaus*val.y;
+          sum_z += gaus*val.z;
+        }
+      dst(dst_y, dst_x).x = sum_x/factor; //b
+      dst(dst_y, dst_x).y = sum_y/factor; //g
+      dst(dst_y, dst_x).z = sum_z/factor; //r
     }}
 
 int divUp(int a, int b)
