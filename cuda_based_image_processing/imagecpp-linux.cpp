64dc9ef74672030ef88da53261ff1780b39fa43d
Shani Israelov <56839113+shani1610@users.noreply.github.com>
2023-03-29 15:00:11 +0200

    Update imagecpp-linux.cpp

--- cuda_based_image_processing/imagecpp-linux.cpp
+++ cuda_based_image_processing/imagecpp-linux.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void startCUDA ( cv::cuda::GpuMat& src,cv::cuda::GpuMat& dst );
+void startCUDA ( cv::cuda::GpuMat& src,cv::cuda::GpuMat& dst, cv::cuda::GpuMat& dst2 );
 
 int main( int argc, char** argv )
 {
@@ -14,11 +14,13 @@ int main( int argc, char** argv )
 
   cv::Mat h_img = cv::imread(argv[1]);
   cv::Mat h_result;
+  cv::Mat h2_result;
 
-  cv::cuda::GpuMat d_img, d_result;
+  cv::cuda::GpuMat d_img, d_result, d2_result;
 
   d_img.upload(h_img);
   d_result.upload(h_img);
+  d2_result.upload(h_img);
   int width= d_img.cols;
   int height = d_img.rows;
 
@@ -29,14 +31,16 @@ int main( int argc, char** argv )
   
   for (int i=0;i<iter;i++)
     {
-      startCUDA ( d_img,d_result );
+      startCUDA ( d_img, d_result, d2_result );
     }
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = end-begin;
 
   d_result.download(h_result);
-  
   cv::imshow("Processed Image", h_result);
+
+  d2_result.download(h2_result);
+    cv::imshow("Processed 2 Image", h2_result);
 
   cout << "Time: "<< diff.count() << endl;
   cout << "Time/frame: " << diff.count()/iter << endl;
